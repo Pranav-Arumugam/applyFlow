658a6220568214c643801e3a234fcab5af228e88
Pranav <pranavcr28@gmail.com>
2026-01-26 09:44:41 +0000

    added frontend build command

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"))
 }
-app.use(express.static(path.resolve(__dirname, "./public")))
+app.use(express.static(path.resolve(__dirname, "./client/dist")))
 app.use(cookieParser())
 app.use(express.json())
 
@@ -41,7 +41,7 @@ app.use("/api/v1/interviews", authenticateUser, interviewRouter)
 app.use("/api/v1/auth", authRouter)
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"))
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"))
 })
 
 app.use("*", (req, res) => {
