43d679c3f1e65832aaff1c8b7a4e0d5f262c4f5e
Pranav <pranavcr28@gmail.com>
2026-04-05 13:55:21 +0100

    test: add hashPassword tests , fix analyseJobFit bug - 34 test total

---
+++ utils/__tests__/analyseJobFit.test.ts
@@ -0,0 +1,67 @@
+import { analyseJobFit } from "../analyzeJD"
+import { extractSkillsByFrequency } from "../analyzeJD"
+import { Skills } from "../../types"
+describe("anaylse Job fit logic", () => {
+  test("should return 0 if no match", () => {
+    const jobDescription =
+      "We are looking for a software engineer with experience in JavaScript and React."
+    const userSkills: Skills[] = [
+      { name: "Python", years: 0 },
+      { name: "Django", years: 1 },
+    ]
+    const requiredSkills = extractSkillsByFrequency(jobDescription, userSkills)
+    // console.log("requiredSkills", requiredSkills)
+    const { matchScore } = analyseJobFit(requiredSkills, userSkills)
+
+    expect(matchScore).toBe(0)
+  })
+
+  test("should return 100 if all match", () => {
+    const jobDescription =
+      "We are looking for a software engineer with experience in JavaScript and React."
+    const userSkills: Skills[] = [
+      { name: "JavaScript", years: 2 },
+      { name: "React", years: 3 },
+    ]
+
+    const requiredSkills = extractSkillsByFrequency(jobDescription, userSkills)
+    // console.log("requiredSkills", requiredSkills)
+    const {
+      matchedSkills,
+      missingSkills,
+      matchScore,
+      totalRequired,
+      totalMatched,
+    } = analyseJobFit(requiredSkills, userSkills)
+    // console.log("matchedSkills", matchedSkills)
+    // console.log("missingSkills", missingSkills)
+    // console.log("totalRequired", totalRequired)
+    // console.log("totalMatched", totalMatched)
+    // console.log("matchScore", matchScore)
+    expect(matchScore).toBe(100)
+  })
+
+  test("check for no job description value", () => {
+    const jobDescription = ""
+    const userSkills: Skills[] = [
+      { name: "JavaScript", years: 2 },
+      { name: "React", years: 3 },
+    ]
+
+    const requiredSkills = extractSkillsByFrequency(jobDescription, userSkills)
+    console.log("requiredSkills", requiredSkills)
+    const {
+      matchedSkills,
+      missingSkills,
+      matchScore,
+      totalRequired,
+      totalMatched,
+    } = analyseJobFit(requiredSkills, userSkills)
+    // console.log("matchedSkills", matchedSkills)
+    // console.log("missingSkills", missingSkills)
+    // console.log("totalRequired", totalRequired)
+    // console.log("totalMatched", totalMatched)
+    // console.log("matchScore", matchScore)
+    expect(matchScore).toBe(0)
+  })
+})
---
+++ utils/__tests__/hashPassword.test.ts
@@ -0,0 +1,42 @@
+import { hashPassword, checkPassword } from "../hashPassword"
+
+describe("testing hashPassword function", () => {
+  test("should hash the string into a different string", async () => {
+    const password = "mysecretpassword"
+    const hashed = await hashPassword(password)
+    expect(hashed).not.toBe(password)
+  })
+
+  test("should return a string of lenght greater than 10", async () => {
+    const password = "mysecretpassword"
+    const hashed = await hashPassword(password)
+    expect(typeof hashed).toBe("string")
+    expect(hashed.length).toBeGreaterThan(10)
+  })
+
+  test("should return a different hash for the same password", async () => {
+    const password = "mysecretpassword"
+    const hashed1 = await hashPassword(password)
+    const hashed2 = await hashPassword(password)
+    expect(hashed1).not.toBe(hashed2)
+  })
+})
+
+describe("testing checkPassword function", () => {
+  const password = "mysecretpassword"
+  let hashedPassword: string = ""
+
+  beforeAll(async () => {
+    hashedPassword = await hashPassword(password)
+  })
+
+  test("should return true of correct password", async () => {
+    const isMatch = await checkPassword(password, hashedPassword)
+    expect(isMatch).toBe(true)
+  })
+
+  test("should return false for incorrect password", async () => {
+    const isMatch = await checkPassword("wrongpassword", hashedPassword)
+    expect(isMatch).toBe(false)
+  })
+})
